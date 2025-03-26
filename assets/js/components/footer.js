cf81ee1c97a1a707a2183fab4398c006e28462db
lucasgamadev <lucas.da.gama.lima@gmail.com>
2025-03-25 22:05:08 -0300

    Refatoro o componente do rodapé para Web Component
    Substituo o carregamento do rodapé via `fetch` de um arquivo HTML estático pela implementação de um Web Component (`footer-component`).
    
    Essa abordagem encapsula a estrutura e o comportamento do rodapé, melhorando a modularidade e a reutilização em diferentes partes da aplicação. Removo o arquivo HTML estático anterior (`components/footer.html`) e o script de carregamento correspondente.

---
+++ assets/js/components/footer.js
@@ -0,0 +1,25 @@
+class FooterComponent extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <footer class="mt-16 text-center text-white opacity-80">
+        <h1 class="text-4xl md:text-5xl font-bold text-white mb-4 drop-shadow-lg"></h1>
+        
+        <!-- Botão Sobre no Footer -->
+        <div class="mt-4 mb-4">
+          <a href="/pages/about.html" class="inline-flex items-center bg-white bg-opacity-20 text-white px-4 py-2 rounded-full hover:bg-opacity-30 transition-all">
+            <i class="fas fa-info-circle mr-2"></i>
+            <span>Sobre</span>
+          </a>
+        </div>
+        
+        <p>2025 SuperCalc - Todas as calculadoras que você precisa em um só lugar</p>
+      </footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', FooterComponent); 
